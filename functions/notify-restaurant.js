e4025fbb944ebc8b5beb0ad027fda67ff02de623
mehras900 <mehra.siddarth9@gmail.com>
2024-02-01 06:24:18 -0500

    Implementing idempotency in event processing

--- functions/notify-restaurant.js
+++ functions/notify-restaurant.js
@@ -1,12 +1,50 @@
+// const { EventBridgeClient, PutEventsCommand } = require('@aws-sdk/client-eventbridge')
+// const eventBridge = new EventBridgeClient()
+// const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns')
+// const sns = new SNSClient()
+
+// const busName = process.env.bus_name
+// const topicArn = process.env.restaurant_notification_topic
+
+// module.exports.handler = async (event) => {
+//   const order = event.detail
+//   const publishCmd = new PublishCommand({
+//     Message: JSON.stringify(order),
+//     TopicArn: topicArn
+//   })
+//   await sns.send(publishCmd)
+
+//   const { restaurantName, orderId } = order
+//   console.log(`notified restaurant [${restaurantName}] of order [${orderId}]`)
+
+//   const putEventsCmd = new PutEventsCommand({
+//     Entries: [{
+//       Source: 'big-mouth',
+//       DetailType: 'restaurant_notified',
+//       Detail: JSON.stringify(order),
+//       EventBusName: busName
+//     }]
+//   })
+//   await eventBridge.send(putEventsCmd)
+
+//   console.log(`published 'restaurant_notified' event to EventBridge`)
+// }
+
 const { EventBridgeClient, PutEventsCommand } = require('@aws-sdk/client-eventbridge')
 const eventBridge = new EventBridgeClient()
 const { SNSClient, PublishCommand } = require('@aws-sdk/client-sns')
 const sns = new SNSClient()
+const { makeIdempotent } = require('@aws-lambda-powertools/idempotency')
+const { DynamoDBPersistenceLayer } = require('@aws-lambda-powertools/idempotency/dynamodb')
 
 const busName = process.env.bus_name
 const topicArn = process.env.restaurant_notification_topic
 
-module.exports.handler = async (event) => {
+const persistenceStore = new DynamoDBPersistenceLayer({
+  tableName: process.env.idempotency_table
+})
+
+const handler = async (event) => {
   const order = event.detail
   const publishCmd = new PublishCommand({
     Message: JSON.stringify(order),
@@ -28,4 +66,8 @@ module.exports.handler = async (event) => {
   await eventBridge.send(putEventsCmd)
 
   console.log(`published 'restaurant_notified' event to EventBridge`)
+
+  return orderId
 }
+
+module.exports.handler = makeIdempotent(handler, { persistenceStore })
